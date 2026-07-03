87ba7def54c83fa88732083b689e79ae831de721
SSSG7Y <seguragreninja@email.com>
2026-07-03 10:11:35 -0400

    actualizar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,4 +24,4 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-    })->create();
+    })->create(); 
