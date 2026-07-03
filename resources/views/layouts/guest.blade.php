437890b0722eb3ddb0db78ccee7df5ca841f635d
SSSG7Y <seguragreninja@email.com>
2026-07-03 09:59:11 -0400

    actualizar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
